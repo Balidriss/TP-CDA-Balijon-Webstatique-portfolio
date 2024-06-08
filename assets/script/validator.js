b0559cdcb15222742a0567c60a67d1a3ec54290f
Idriss Balijon <I.balijon@gmail.com>
2024-06-08 17:24:49 +0200

    create validator class

---
+++ assets/script/validator.js
@@ -0,0 +1,24 @@
+class Validator {
+
+
+
+    constructor(type, condition) {
+        //
+        //email : check si il y a un @ et un . et max char
+        //
+        //object : minimum et max char
+        //
+        //message : minimum char et max char
+
+
+    }
+
+}
+
+function displayErrorOnElement(element, message) {
+
+}
+
+function createXMLDoc(object) {
+
+};
